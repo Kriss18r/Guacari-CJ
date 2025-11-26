e93c070c32a83a4faff08de29278b84157603d3d
Kris18 <nessrestrepo898@gmail.com>
2025-11-26 16:35:51 -0500

    Refactor admin.py to register models for Post, Category, Event, and Member with appropriate configurations

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Category, Post, Photo, Event, Member
 
 # Register your models here.
+
+class PhotoInLine(admin.TabularInline):
+    model = Photo
+    extra = 1
+
+#Admin para Posts
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published', 'is_published')
+    list_filter = ('is_published', 'category')
+    search_fields = ('title', 'excerpt', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [PhotoInLine]
+    date_hierarchy = 'published'
+    ordering = ('-published',)
+
+#Admin para Categorias
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+#Admin para Eventos
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start', 'end', 'location')
+    list_filter = ('start')
+    search_fields = ('title', 'description', 'location')
+
+#Admin para Miembros
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'role', 'joined')
+    search_fields = ('first_name', 'last_name', 'role')
+
